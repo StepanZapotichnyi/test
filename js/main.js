3aed831da02fb176d08a557a74e9672b5458fa8a
Stepan <stepanzapotichnyi@gmail.com>
2023-05-18 08:21:44 +0200

    First one commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// var leftBorderWidth = 1;
+// var string = 'Hello';
+// var sym = Symbol();
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+// let something = 42;
+// console.log(something);
+
+let person = {
+    name: 'john',
+    age: 20,
+    isMarried: true
+};
+
+// console.log(person['name']);
+// console.log(person.age);
+
+let arr = ['plum.png', 'banana.png', 'apple.png'];
+// console.log(arr[1]);
+
+// //alert("Hello world!");
+
+// // let answer = +prompt('Are you eighteen?', 'Yes');
+// console.log(typeof (answer));
+
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
